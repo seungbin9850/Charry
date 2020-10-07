63219f1111ae6936ff9519d890292a810b39eb0a
yangseungbin <tanja1206@naver.com>
2020-10-07 23:11:04 +0900

    [MODIFY] json in error message

--- src/middlewares/tryCatch.ts
+++ src/middlewares/tryCatch.ts
@@ -10,7 +10,7 @@ export class tryCatchMiddleware {
       try {
         await cb(req, res, next);
       } catch (e) {
-        res.status(e.statusCode || 500).end();
+        res.status(e.statusCode || 500).json({ message: e.message });
       }
     };
   };
