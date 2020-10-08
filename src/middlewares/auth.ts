62240022e19f8e5a7b86dd23c6750002436df72d
yangseungbin <tanja1206@naver.com>
2020-10-08 16:44:07 +0900

    [MODIFY] status code 403 -> 401

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -8,9 +8,9 @@ export const authMiddleware = (
 ) => {
   const token: any = req.headers["authorization"];
   const bearer = token.split("Bearer ")[1];
-  if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
+  if (!token) return res.status(401).json({ message: "token required" });
   jwt.verify(bearer, req.app.get("jwt-secret"), (err, decoded) => {
-    if (err) return res.status(403).json({ message: "토큰 만료됨" });
+    if (err) return res.status(403).json({ message: "token expired" });
     req["decoded"] = decoded;
     next();
   });
@@ -23,9 +23,9 @@ export const refreshMiddleware = (
 ) => {
   const token: any = req.headers["authorization"];
   const bearer = token.split("Bearer ")[1];
-  if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
+  if (!token) return res.status(401).json({ message: "token required" });
   jwt.verify(bearer, req.app.get("refresh-secret"), (err, decoded) => {
-    if (err) return res.status(403).json({ message: "토큰 만료됨" });
+    if (err) return res.status(403).json({ message: "token expired" });
     req["decoded"] = decoded;
     next();
   });
