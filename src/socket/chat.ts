dc06361fab4892f24a11cf4246c9fb2b97209ab0
yangseungbin <tanja1206@naver.com>
2020-10-12 22:04:02 +0900

    [ADD] destroy room

--- src/socket/chat.ts
+++ src/socket/chat.ts
@@ -2,19 +2,22 @@ import * as ChatService from "../services/chat";
 import * as RoomService from "../services/room";
 
 const socketEvent = (io, socket) => {
-  socket.on("join", async (roomId, nickname) => {
+  socket.on("join", async (roomId: string, nickname: string) => {
     await RoomService.joinMember(roomId, socket.decoded_token.id);
     socket.join(roomId);
     io.to(roomId).emit("joinMessage", nickname);
   });
-  socket.on("chat", async (roomId, content) => {
+  socket.on("chat", async (roomId: string, content: string) => {
     await ChatService.chat(roomId, socket.decoded_token.id, content);
     io.to(roomId).emit("chatMessage", content);
   });
-  socket.on("leave", async (roomId, nickname) => {
-    await RoomService.outUser(roomId, socket.decoded_token.id);
+  socket.on("leave", async (roomId: string, nickname: string) => {
+    await RoomService.outUser(socket.decoded_token.id, roomId);
     socket.leave(roomId);
     io.to(roomId).emit("leaveMessage", nickname);
+  });
+  socket.on("destroy", async (roomId: string) => {
+    await RoomService.destroyOne(socket.decoded_token.id, roomId);
   });
   socket.on("disconnect");
 };
