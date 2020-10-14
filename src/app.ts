de562f6f0c968b3b34fbe62fbf8d1e651dcd31a1
yangseungbin <tanja1206@naver.com>
2020-10-14 18:23:27 +0900

    [ADD] app.ts socket io

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ import http from "http";
 import socketio from "socket.io";
 import { sequelize } from "./config/config";
 import router from "./routes";
-import chat from "./socket/chat";
+import * as ChatService from "./services/chat";
+import * as RoomService from "./services/room";
 
 dotenv.config({ path: path.join(__dirname + "../../.env") });
 
@@ -34,7 +35,27 @@ app.set("jwt-secret", process.env.JWT_SECRET);
 app.set("refresh-secret", process.env.REFRESH_SECRET);
 
 io.on("connection", (socket) => {
-  chat(io, socket);
+  socket.on("join", async (roomId: string, email: string, nickname: string) => {
+    await RoomService.joinMember(roomId, email);
+    socket.join(roomId);
+    io.to(roomId).emit("joinMessage", nickname);
+  });
+  socket.on("chat", async (roomId: string, email: string, content: string) => {
+    await ChatService.chat(roomId, email, content);
+    io.to(roomId).emit("chatMessage", content);
+  });
+  socket.on(
+    "leave",
+    async (roomId: string, email: string, nickname: string) => {
+      await RoomService.outUser(email, roomId);
+      socket.leave(roomId);
+      io.to(roomId).emit("leaveMessage", nickname);
+    }
+  );
+  socket.on("destroy", async (roomId: string, email: string) => {
+    await RoomService.destroyOne(email, roomId);
+  });
+  socket.on("disconnect");
 });
 
 server.listen(8000, () => {
