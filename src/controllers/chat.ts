ec1c3dce9ed2001f0efac2a60a2e58864648b47a
yangseungbin <tanja1206@naver.com>
2020-12-04 17:18:08 +0900

    [ADD] show chat logs

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -9,6 +9,10 @@ export const showChatLog = async (
   const roomId: string = req.params.id;
   const userId: string = req["decoded"].id;
   const page: number = Number(req.params.page);
-  const chats: Array<object> = await ChatService.showLog(roomId, userId, page);
+  const chats: Array<object> = await ChatService.showLog({
+    roomId,
+    userId,
+    page,
+  });
   res.status(200).json({ chats });
 };
